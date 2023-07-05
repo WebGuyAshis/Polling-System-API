6560ed51616898572f4c3f42c8eae28f0f143683
Ashis <developer.ashis99@gmail.com>
2023-07-06 01:18:20 +0530

    Removed Deployment

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
 
 
-// mongoose.connect('mongodb://127.0.0.1:27017/polling-system-api')
-mongoose.connect(process.env.mongooseUrl);
+mongoose.connect('mongodb://127.0.0.1:27017/polling-system-api')
 
 const db = mongoose.connection;
 
