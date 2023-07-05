0d31a8210d54c682a17b293dbc82105b5663a853
Ashis <developer.ashis99@gmail.com>
2023-07-06 00:59:26 +0530

    Added env and fixed bugs

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect('mongodb://127.0.0.1:27017/polling-system-api')
+
+// mongoose.connect('mongodb://127.0.0.1:27017/polling-system-api')
+mongoose.connect(process.env.mongooseUrl);
 
 const db = mongoose.connection;
 
--- controllers/questionsController.js
+++ controllers/questionsController.js
@@ -1,5 +1,5 @@
 const Questions = require('../models/Questions');
-const { options } = require('../routes');
+const Options = require('../models/Options')
 // Viewing Question
 module.exports.question = (req, res) => {
     const questionId = req.params.questionId;
@@ -47,31 +47,30 @@ module.exports.delete = (req, res) => {
     const questionId = req.params.questionId;
   
     Questions.findById(questionId)
-      .populate('options')
       .then(question => {
         if (question) {
           const hasVotes = question.options.some(option => option.votes > 0);
           if (hasVotes) {
             return res.status(400).json({ message: 'Cannot delete question with options having votes' });
           }
-          question.deleteOne()
-          .then(() => {
-            return res.status(200).json({ message: 'Successfully Deleted Question!!' });
-          })
-          .catch(err => {
-            console.log('Error deleting question', err);
-            return res.status(500).json({ message: 'Error deleting question' });
-          });
-          return res.status(200).json({ message: 'Successfully Deleted Question!!' });
+          return Options.deleteMany({ _id: { $in: question.options } }); // Delete associated options
         } else {
           return res.status(404).json({ message: 'Question Not Found!' });
         }
       })
+      .then(() => {
+        return Questions.deleteOne({ _id: questionId }); // Delete the question
+      })
+      .then(() => {
+        return res.status(200).json({ message: 'Successfully Deleted Question!!' });
+      })
       .catch(err => {
-        console.log('Internal Server Error!', err);
-        return res.status(500).json({ message: 'Internal Server Error' });
+        console.log('Error deleting question', err);
+        return res.status(500).json({ message: 'Error deleting question' });
       });
   };
+  
+  
   
     // Questions.findByIdAndDelete(questionId)
     //     .then(question=>{
